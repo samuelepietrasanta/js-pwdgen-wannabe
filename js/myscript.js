35877c3663bb5806de5def15db44c8dd04fa48ae
samuelepietrasanta <samuele.pietrasanta@gmail.com>
2021-08-04 15:49:26 +0200

    generatore password finished

---
+++ js/myscript.js
@@ -0,0 +1,5 @@
+var nome = prompt("Inserisci il tuo nome");
+var cognome = prompt("Inserisci il tuo cognome");
+var colorePreferito = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById("password-generata").innerHTML = nome + cognome + colorePreferito + "21";
